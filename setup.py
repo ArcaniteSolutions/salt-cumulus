bc8625b313625ca89ca90d496f757ed557554005
Maximilien Cuony <maximilien@theglu.org>
2019-05-10 11:41:26 +0200

    Hotfix: Add description and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
     name="salt-cumulus",
     version="0.0.1",
@@ -7,9 +12,19 @@ setup(
     author="Maximilien Cuony",
     author_email="maximilien.cuony@arcanite.ch",
     url='https://github.com/ArcaniteSolutions/salt-cumulus',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires='sh',
     license="MPL",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: Other OS",
+        "Programming Language :: Python",
+        "Topic :: System :: Networking",
+    ],
     entry_points='''
     [salt.loader]
     states_dirs = salt_cumulus.loader:states_dirs
